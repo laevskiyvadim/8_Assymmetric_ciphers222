4d068c697a5387292dbe745f29ea771b401f58bc
Vadim Laevskiy <laevskiy.vadim@mail.ru>
2024-05-16 07:25:18 +0300

    aaaa

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
-import socket
 import pickle
+from dotenv import load_dotenv
+import socket
+import os
+from helpers import power
+load_dotenv()
 
-HOST = '127.0.0.1'
-PORT = 8080
 
 sock = socket.socket()
-sock.connect((HOST, PORT))
+sock.connect((os.getenv('HOST'), int(os.getenv('PORT'))))
 
 p, g, a = 7, 5, 3
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+msg = sock.recv(1024)
+B = pickle.loads(msg)
+K = B ** a % p
+print("K =", K)
 
 sock.close()
-
--- server.py
+++ server.py
@@ -1,16 +1,21 @@
-import socket
 import pickle
-
-HOST = '127.0.0.1'
-PORT = 8080
+from dotenv import load_dotenv
+import socket
+import os
+from helpers import power
+load_dotenv()
 
 sock = socket.socket()
-sock.bind((HOST, PORT))
+sock.bind((os.getenv('HOST'), int(os.getenv('PORT'))))
 sock.listen(1)
 conn, addr = sock.accept()
 
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+b = 9
+p, g, A = pickle.loads(msg)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+K = A ** b % p
+print("K =", K)
 
 conn.close()
-

1f4997800dd7cd420dfe3ecb6e0bbe5442a635a1
Vadim Laevskiy <laevskiy.vadim@mail.ru>
2024-05-29 13:12:17 +0300

    tram pam pam

--- client.py
+++ client.py
@@ -2,25 +2,33 @@ import pickle
 from dotenv import load_dotenv
 import socket
 import os
-from helpers import power
 load_dotenv()
 
 
 sock = socket.socket()
 sock.connect((os.getenv('HOST'), int(os.getenv('PORT'))))
 
+
 p, g, a = 7, 5, 3
+# генерируем ключ диффи-хелфмана
 A = g ** a % p
+# отправляем на сервер
 sock.send(pickle.dumps((p, g, A)))
+# получаем от сервера B
 msg = sock.recv(1024)
 B = pickle.loads(msg)
+# проверяем правильность генерации пары ключей
 K = B ** a % p
 print("K =", K)
 
+# вводим сообщение
 message = input()
+# конвертируем каждый символ сообщения в его байтовое представление
 bits = list(map(ord, list(message)))
+# к каждому байту прибавляем смещение K
 bits = [i+K for i in bits]
 
+# склеиваем символы по пробелу и передаём на сервер
 message = ' '.join(list(map(str, bits)))
 sock.send(message.encode())
 
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import pickle
 from dotenv import load_dotenv
 import socket
 import os
-from helpers import power
 load_dotenv()
 
 sock = socket.socket()
@@ -10,21 +9,27 @@ sock.bind((os.getenv('HOST'), int(os.getenv('PORT'))))
 sock.listen(1)
 conn, addr = sock.accept()
 
+# получаем от клиента ключ и свободные числа
 msg = conn.recv(1024)
 b = 9
 p, g, A = pickle.loads(msg)
+# генерируем B и отправляем на клиента
 B = g ** b % p
 conn.send(pickle.dumps(B))
 K = A ** b % p
 print("K =", K)
 
-
+# получаем от клиента защифрованное сообщение
 message = conn.recv(1024)
 
 message = message.decode()
+# разделяем сообщение по пробелу и конвертируем каждый символ в число
 message = list(map(int, message.split()))
+# делаем обратное смещение кода символа
 message = [i-K for i in message]
+# преобразуем кодовые представления символов в символы
 message = ''.join(list(map(chr, message)))
+
 print(message)
 
 conn.close()
